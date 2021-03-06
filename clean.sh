d9af0d1b533b5f810a26e6722dd77f3af77b8abf
彪大大 <imtyb@qq.com>
2021-03-06 09:54:34 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch logo.png' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch logo-only.png' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch article/make_idea_fast-1.png' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch article/make_idea_fast-2.png' --prune-empty --tag-name-filter cat -- --all
 
 #上面的索引删除了之后需要执行强制推送
 #git push origin wp --force
